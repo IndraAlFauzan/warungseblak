6532e0533e4e8af9b3aa4e6aaad58802b9d50a4a
IndraAlFauzan <nurulindra35@gmail.com>
2025-01-07 16:48:13 +0700

    route spicy level

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\FlavorController;
+use App\Http\Controllers\Api\SpicyLevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -32,6 +33,14 @@ Route::middleware(['json','auth:api', 'role:admin' ])->group(function () {
     Route::get('/flavors/{id}', [FlavorController::class, 'show']);
     Route::put('/flavors/{id}', [FlavorController::class, 'update']);
     Route::delete('/flavors/{id}', [FlavorController::class, 'destroy']);
+});
+
+Route::middleware(['json','auth:api', 'role:admin' ])->group(function () {
+    Route::get('/spicy-levels', [SpicyLevelController::class, 'index']);
+    Route::post('/spicy-levels', [SpicyLevelController::class, 'store']);
+    Route::get('/spicy-levels/{id}', [SpicyLevelController::class, 'show']);
+    Route::put('/spicy-levels/{id}', [SpicyLevelController::class, 'update']);
+    Route::delete('/spicy-levels/{id}', [SpicyLevelController::class, 'destroy']);
 });
 
 
