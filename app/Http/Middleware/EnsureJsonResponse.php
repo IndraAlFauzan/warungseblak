8b1ba097ecc64cafc8c2284096eba66d1db87285
IndraAlFauzan <nurulindra35@gmail.com>
2025-01-07 12:34:03 +0700

    fix auth

--- app/Http/Middleware/EnsureJsonResponse.php
+++ app/Http/Middleware/EnsureJsonResponse.php
@@ -16,21 +16,26 @@ class EnsureJsonResponse
     public function handle(Request $request, Closure $next)
     {
         // Cek jika header Accept tidak berisi application/json
-        if (!$request->expectsJson()) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Accept header must be application/json',
-            ], 406); // HTTP 406 Not Acceptable
-        }
+        // Paksa penggunaan Accept: application/json
+        // if (!$request->hasHeader('Accept') || $request->header('Accept') !== 'application/json') {
+        //     $request->headers->set('Accept', 'application/json');
+        // }
 
         $response = $next($request);
 
         // Jika respons bukan JSON, tambahkan Content-Type header
         if ($response instanceof \Symfony\Component\HttpFoundation\Response) {
             $contentType = $response->headers->get('Content-Type');
+            $accept = $response->headers->get('Accept');
+            if (strpos($accept, 'application/json') === false) {
+                $response->headers->set('Accept', 'application/json');
+            }
             if (strpos($contentType, 'application/json') === false) {
                 $response->headers->set('Content-Type', 'application/json');
             }
+
+            
+            
         }
 
         return $response;
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('json','auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
